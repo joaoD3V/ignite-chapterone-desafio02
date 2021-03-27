5b3194a07c20cb901abb823ad9f3a30004e419cb
Joao Pedro <joao.devweb@gmail.com>
2021-03-27 14:05:23 -0300

    Challenge Completed

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,45 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+import { Button } from '../components/Button';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface GenreProps {
+  onSelectID: (id: number) => void;
+  selectedGenreId: number;
+}
+
+import { api } from '../services/api';
+
+export function SideBar({ onSelectID, selectedGenreId }: GenreProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+
+
+  return(
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onSelectID(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+  </nav>
+  );
 }
